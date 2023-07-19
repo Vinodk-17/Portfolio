90c96bfc4b4e8c3feaf51f2130459c1bb8198852
Vinod <kurilvinod9870@gmail.com>
2023-07-19 20:58:31 +0530

    ,

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,57 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import Particle from "../Particle";
+// import Github from "./Github";
+// import Techstack from "./Techstack";
+// import Aboutcard from "./AboutCard";
+// import laptopImg from "../../Assets/about1.jpeg";
+// import Toolstack from "./Toolstack";
+
+// function About() {
+//   return (
+//     <Container fluid className="about-section">
+//       <Particle />
+//       <Container>
+//         <Row style={{ justifyContent: "center", padding: "10px" }}>
+//           <Col
+//             md={7}
+//             style={{
+//               justifyContent: "center",
+//               paddingTop: "30px",
+//               paddingBottom: "50px",
+//             }}
+//           >
+//             <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+//               Know Who <strong className="purple">I'M</strong>
+//             </h1>
+//             <Aboutcard />
+//           </Col>
+//           <Col
+//             md={5}
+//             style={{ paddingTop: "120px", paddingBottom: "50px" }}
+//             className="about-img"
+//           >
+//             <img src={laptopImg} alt="about" className="img-fluid" />
+//           </Col>
+//         </Row>
+//         <h1 className="project-heading">
+//           Professional <strong className="purple">Skillset </strong>
+//         </h1>
+
+//         <Techstack />
+
+//         <h1 className="project-heading">
+//           <strong className="purple">Tools</strong> I use
+//         </h1>
+//         <Toolstack />
+
+//         {/* <Github /> */}
+//       </Container>
+//     </Container>
+//   );
+// }
+
+// export default About;
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
@@ -28,10 +82,24 @@ function About() {
           </Col>
           <Col
             md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            style={{
+              paddingTop: "120px",
+              paddingBottom: "50px",
+              position: "relative",
+            }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" />
+            <img
+              src={laptopImg}
+              alt="about"
+              className="img-fluid"
+              style={{
+                transition: "opacity 0.3s, transform 0.3s",
+                opacity: "1",
+              }}
+              onMouseOver={(e) => (e.target.style.opacity = "0.7")}
+              onMouseOut={(e) => (e.target.style.opacity = "1")}
+            />
           </Col>
         </Row>
         <h1 className="project-heading">
